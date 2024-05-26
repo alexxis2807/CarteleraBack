483671f8ec6253b3ae3f86fc3e702d5accde07ea
Alexander <alexisparedes_2019@hotmail.com>
2024-05-26 21:56:36 +0200

    Metodos para obtener las sesiones de una Pelicula

--- src/main/java/com/cartelera/controladores/SesionPeliculaControlador.java
+++ src/main/java/com/cartelera/controladores/SesionPeliculaControlador.java
@@ -1,13 +1,15 @@
 package com.cartelera.controladores;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,6 +43,16 @@ public class SesionPeliculaControlador {
     @GetMapping("/") 
     ResponseEntity<List<SesionPelicula>> obtenerSesiones(){
         return ResponseEntity.ok(sesionPeliculaRepositorio.findAll());
+    }
+
+    @GetMapping("/fecha/{fecha}/id/{idPelicula}") 
+    ResponseEntity<List<SesionPelicula>> obtenerSesionesPeliculaFecha(@PathVariable("fecha") LocalDate fecha, @PathVariable("idPelicula") Long idPelicula){
+        return ResponseEntity.ok(this.sesionPeliculaRepositorio.encuentraSesionesFechaPelicula(fecha, idPelicula));
+    }
+
+    @GetMapping("/id/{idPelicula}") 
+    ResponseEntity<Set<LocalDate>> obtenerFechasSesionesPelicula(@PathVariable("idPelicula") Long idPelicula){
+        return ResponseEntity.ok(this.sesionPeliculaRepositorio.encuentraFechasSesionesPelicula(idPelicula));
     }
 
 }
--- src/main/java/com/cartelera/repositorios/SesionPeliculaRepositorio.java
+++ src/main/java/com/cartelera/repositorios/SesionPeliculaRepositorio.java
@@ -3,6 +3,7 @@ package com.cartelera.repositorios;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -16,4 +17,9 @@ public interface SesionPeliculaRepositorio extends JpaRepository<SesionPelicula,
 
     @Query("SELECT sesion FROM SesionPelicula sesion WHERE sesion.fecha = :fecha AND sesion.sala.id = :sala AND ((sesion.horaInicio BETWEEN :horaInicio AND :horaFin) OR (sesion.horaFin BETWEEN :horaInicio AND :horaFin)) ")
     List<SesionPelicula> encuentraSesionPorFechaYSala(@Param("sala") Long sala, @Param("fecha") LocalDate fecha, @Param("horaInicio") LocalTime horaInicio, @Param("horaFin") LocalTime horaFin);
+    @Query("SELECT sesion FROM SesionPelicula sesion WHERE sesion.fecha = :fecha AND  sesion.pelicula.id = :idPelicula")
+    List<SesionPelicula> encuentraSesionesFechaPelicula(@Param("fecha") LocalDate fecha, @Param("idPelicula") Long idPelicula);
+    @Query("SELECT sesion.fecha FROM SesionPelicula sesion WHERE sesion.pelicula.id = :idPelicula ORDER BY sesion.fecha")
+    Set<LocalDate> encuentraFechasSesionesPelicula(@Param("idPelicula") Long idPelicula);
+
 }
